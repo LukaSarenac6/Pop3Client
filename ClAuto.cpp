e202e42cc6b826ebcd962e7d768009cc7d4a9191
Luka Sarenac <123103318+LukaSarenac6@users.noreply.github.com>
2024-01-13 01:06:32 +0100

    Add files via upload

---
+++ ClAuto.cpp
@@ -0,0 +1,115 @@
+#include "ClAuto.h"
+
+ClAuto::ClAuto(QObject *parent) {
+    clientState = FSM_Cl_Ready;
+}
+
+ClAuto::~ClAuto() {
+
+}
+
+QString ClAuto::Client_Make_Message(QString& string) {
+
+    QString message = "MSG(" + string + ")";
+    return message;
+}
+
+void ClAuto::Client_Slot_User_Check_Mail(const QString& userMessage) {
+    //qDebug() << "Received message = " << userMessage;
+
+    // Check if message is correct
+    if (userMessage != "User_Check_Mail") {
+        qDebug() << "ERROR MESSAGE INCORRECT IN Client_Slot_User_Check_Mail";
+        return;
+    }
+
+    const QString clientMessage = "Cl_Connection_Request";
+
+    emit Client_Signal_Connection_Request(clientMessage);
+
+    clientState = FSM_Cl_Connecting;
+}
+
+void ClAuto::Client_Slot_Channel_Connection_Accept(const QString& channelMessage) {
+    //qDebug() << "Channel message = " << channelMessage;
+    QString clientMessage;
+
+    if (channelMessage == "Cl_Connection_Reject") {
+        clientMessage = "User_Connection_Fail";
+
+        emit Client_Signal_User_Connection(clientMessage);
+
+        clientState = FSM_Cl_Ready;
+    } else if (channelMessage == "Cl_Connection_Accept") {
+        clientMessage = "User_Connected";
+
+        emit Client_Signal_User_Connection(clientMessage);
+
+        clientState = FSM_Cl_Authorising;
+    } else {
+        qDebug() << "ERROR MESSAGE INCORECT IN Client_Slot_Channel_Connection_Accept";
+    }
+}
+
+void ClAuto::Client_Slot_User_Sent_Username_Password(const QString& username, const QString& password) {
+
+    if (!usernameSent) {
+        qDebug() << username;
+        qDebug() << password;
+
+        usernameReceived = username;
+        passwordReceived = password;
+    }
+
+    QString usernameMessage = usernameReceived;
+    QString passwordMessage = passwordReceived;
+
+    if (!usernameSent) {
+        usernameSent = true;
+
+        usernameMessage = Client_Make_Message(usernameMessage);
+        qDebug() << usernameMessage;
+
+        emit Client_Signal_Send_Username(usernameMessage);
+
+        clientState = FSM_Cl_User_Check;
+        return;
+    }
+
+    if (!passwordSent) {
+        passwordSent = true;
+
+        passwordMessage = Client_Make_Message(passwordMessage);
+        qDebug() << passwordMessage;
+
+        emit Client_Signal_Send_Password(passwordMessage);
+        clientState = FSM_Cl_Pass_Check;
+        return;
+    }
+
+    //qDebug() << "za sad radi dobro";
+    const QString& statMessage = "MSG(STAT)";
+    emit Client_Signal_Send_Stat(statMessage);
+    clientState = FSM_Cl_Mail_Check;
+}
+
+void ClAuto::Client_Slot_Channel_MSG_Response(const QString messageResponse) {
+    qDebug() << messageResponse;
+    if (messageResponse == "MSG(-ERR)") {
+        // TO DO: MSG QUIT
+        usernameSent = false;
+        passwordSent = false;
+        qDebug() << quitMessage;
+        emit Client_Signal_MSG_Quit(quitMessage);
+        clientState = FSM_Cl_Disconnecting;
+        return;
+    } else if (messageResponse == "Cl_Disconected") {
+        const QString& disconectMessage = "User_Disconected";
+        emit Client_Signal_User_Disconected(disconectMessage);
+        clientState = FSM_Cl_Ready;
+        return;
+    }
+
+    emit Client_Signal_Username_Ok(usernameReceived, passwordReceived);
+}
+
---
+++ UserAuto.cpp
@@ -0,0 +1,45 @@
+#include "UserAuto.h"
+
+UserAuto::UserAuto(QObject *parent) {
+    userState = FSM_User_Idle;
+}
+
+UserAuto::~UserAuto() {
+
+}
+
+
+void UserAuto::User_Set_All() {
+    const QString& message = "User_Check_Mail";
+
+    emit User_Signal_Set_All(message);
+
+    userState = FSM_User_Connecting;
+}
+
+void UserAuto::User_Slot_User_Connection(const QString& clientMessage){
+    //qDebug() <<"Client message = " << clientMessage;
+    if (clientMessage == "User_Connection_Fail") {
+        qDebug() << "CONNECTION REJECTED";
+        userState = FSM_User_Idle;
+        return;
+    }
+
+    emit User_Signal_User_Connected();
+}
+
+void UserAuto::User_Slot_User_Connected() {
+
+    const QString& username = "lana";
+    const QString& password = "1234";
+    // TO DO: unos sa tastature
+    emit User_Signal_Send_Username_Password(username, password);
+
+    userState = FSM_User_Connected;
+
+}
+
+void UserAuto::User_Slot_User_Disconected(const QString& disconectMessage) {
+    qDebug() << disconectMessage;
+    userState = FSM_User_Idle;
+}
